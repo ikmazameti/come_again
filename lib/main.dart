64b38701d148904059f75275bc2c545481f4aa5f
sentinelbot <ikmazameti@gmail.com>
2021-09-11 04:50:55 +0000

    Routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,45 +1,13 @@
+import 'package:come_again/screens/choose_location.dart';
+import 'package:come_again/screens/home.dart';
+import 'package:come_again/screens/loading.dart';
 import 'package:flutter/material.dart';
 
-import 'quote.dart';
-import 'quote_card.dart';
-//List
-
-void main() => runApp(MaterialApp(home: QuoteList()));
-
-class QuoteList extends StatefulWidget {
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote('Awesome Quotes', 'isaac Ayo'),
-    Quote('Awesome Quotes', 'isaac Ayo'),
-    Quote('Awesome Quotes', 'isaac Ayo'),
-    Quote('Awesome Quotes', 'isaac Ayo'),
-    Quote('Awesome Quotes', 'isaac Ayo'),
-    Quote('Awesome Quotes', 'isaac Ayo'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text(
-          'Awesome Quotes',
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          children: quotes.map((quote) {
-            return QuoteCard(quote: quote);
-          }).toList(),
-        ),
-      ),
-    );
-  }
-}
+void main() => runApp(MaterialApp(
+      initialRoute: '/home',
+      routes: {
+        '/': (ct) => Loading(),
+        '/home': (ct) => Home(),
+        '/location': (ct) => ChooseLocation(),
+      },
+    ));
