963bc34ed799766afdda56c352c99c31ade0ffd2
sentinelbot <ikmazameti@gmail.com>
2021-09-11 03:53:37 +0000

    separated card into its module

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 
 import 'quote.dart';
+import 'quote_card.dart';
 //List
 
 void main() => runApp(MaterialApp(home: QuoteList()));
 
 class QuoteList extends StatefulWidget {
-  const QuoteList({Key? key}) : super(key: key);
-
   @override
   _QuoteListState createState() => _QuoteListState();
 }
@@ -21,36 +20,6 @@ class _QuoteListState extends State<QuoteList> {
     Quote('Awesome Quotes', 'isaac Ayo'),
     Quote('Awesome Quotes', 'isaac Ayo'),
   ];
-
-  Card quoteTemplate(quote) {
-    return Card(
-        margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-        child: Padding(
-          padding: const EdgeInsets.all(12.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Text(
-                quote.text,
-                style: TextStyle(
-                  fontSize: 18.0,
-                  color: Colors.grey[600],
-                ),
-              ),
-              SizedBox(
-                height: 6.0,
-              ),
-              Text(
-                quote.text,
-                style: TextStyle(
-                  fontSize: 14.0,
-                  color: Colors.grey[800],
-                ),
-              ),
-            ],
-          ),
-        ));
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,7 +36,7 @@ class _QuoteListState extends State<QuoteList> {
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: quotes.map((quote) {
-            return quoteTemplate(quote);
+            return QuoteCard(quote: quote);
           }).toList(),
         ),
       ),
